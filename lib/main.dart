5d0d8a5a98ff8dc2dfc38a96fd82875ba8d22cc9
Itthipon Treetodbadee <>
2020-06-10 21:35:28 +0700

    edit main

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demooooo Home Page'),
+      home: MyHomePage(title: 'Flutter Demooooo Homeeee Page'),
     );
   }
 }
